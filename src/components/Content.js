cc5197f24a91127c5c220a13bbf45de447a6e3c3
cascharly <cascharly@gmail.com>
2021-03-21 01:05:54 -0300

    icon v2

---
+++ src/components/Content.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Content = () => {
+  return (
+    <>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. A natus ipsum
+        alias dignissimos magnam, error quibusdam autem. Architecto, tenetur
+        fugiat tempora ratione nisi iure tempore facilis, velit inventore,
+        ducimus laudantium?
+      </p>
+    </>
+  );
+};
--- src/components/MainBody.js
+++ src/components/MainBody.js
@@ -1,24 +1,19 @@
-import React from 'react'
-import { SideBar } from './SideBar'
+import React from "react";
+import { Content } from "./Content";
+import { SideBar } from "./SideBar";
 
 export const MainBody = () => {
-    return (
-        <>
-        <div className="row">
-          <div className="col-2 bg-primary">
-            <SideBar />
-          </div>
-         
-          <div className="col bg-warning">
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. A natus
-              ipsum alias dignissimos magnam, error quibusdam autem. Architecto,
-              tenetur fugiat tempora ratione nisi iure tempore facilis, velit
-              inventore, ducimus laudantium?
-            </p>
-          </div>
-        
+  return (
+    <>
+      <div id="mainBody" className="row ">
+        <div className="col-2 px-0">
+          <SideBar />
         </div>
-      </>
-    )
-}
+
+        <div className="col bg-light ">
+          <Content />
+        </div>
+      </div>
+    </>
+  );
+};
