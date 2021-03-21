1af9def9fe72011d714e9ce4f2b1859fba665753
cascharly <cascharly@gmail.com>
2021-03-21 01:24:01 -0300

    notificaciones

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -83,14 +83,14 @@ export const SideBar = () => {
         </h6>
         {/* //////////////////////////////////////////////////////////// */}
         <hr/>
-        <div>
-          <div className="col p-0">
+        <div className="row px-3">
+          <div id="icon-sidebar">
             <svg viewBox="0 0 24 24" fill="#FFFFFF">
               <path d="M12 22c1.1 0 2-.9 2-2h-4c0 1.1.89 2 2 2zm6-6v-5c0-3.07-1.64-5.64-4.5-6.32V4c0-.83-.67-1.5-1.5-1.5s-1.5.67-1.5 1.5v.68C7.63 5.36 6 7.92 6 11v5l-2 2v1h16v-1l-2-2z"></path>
             </svg>
           </div>
-          <div className="col p-0">
-          <h6>Notificaciones</h6>
+          <div className="col">
+          <h6 className="m-1">Notificaciones</h6>
           </div>
         </div>
       </div>
